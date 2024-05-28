7832250e180275d251c31b55f7565a5f2b0f68fd
Abhinav Chahal <85961184+Abhinavchahal@users.noreply.github.com>
2024-05-28 13:41:02 +0530

    To do List project

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,33 @@
+export const DUMMY_USERS = [
+    {
+        id: 'u1',
+        name: 'Abhinav',
+        avatar: 'user-1.jpg',
+    },
+    {
+        id: 'u2',
+        name: 'Chahal',
+        avatar: 'user-2.jpg',
+    },
+    {
+        id: 'u3',
+        name: 'Abhi',
+        avatar: 'user-3.jpg',
+    },
+    {
+        id: 'u4',
+        name: 'Nav',
+        avatar: 'user-4.jpg',
+    },
+
+    {
+        id: 'u5',
+        name: 'Chaudhary',
+        avatar: 'user-5.jpg',
+    },
+    {
+        id: 'u6',
+        name: 'Abhin',
+        avatar: 'user-6.jpg',
+    },
+]
